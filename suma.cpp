118f5a4eeee669ae233c8f83ff57b9a14346b5d3
neylamahmoudi <todaypostres@gmail.com>
2022-03-01 21:34:09 -0400

    first commit

---
+++ suma.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int numero1;
+    int numero2;
+
+    cout << "Ingrese primer valor " << endl;
+    cin >> numero1;
+
+    cout << "Ingrese segundo valor " << endl;
+    cin >> numero2;
+
+    cout << "El resultado de la suma es: " << (numero1 + numero2) << endl;
+
+    return 0;
+}
